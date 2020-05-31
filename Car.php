85b1aa7e0598eeb51c9b25eb62dd1f16a1fc8780
Fábio Costa <fabiodanielmc@gmail.com>
2020-05-31 19:33:29 +0200

    basic object in php

--- Car.php
+++ Car.php
@@ -3,16 +3,29 @@
 class Car {
     public $color;
     public $year;
+    public $model;
 
     public function run(){
-        return "Car is running...";
+        return $this->model . " Car is running...";
     }
 
     public function stop(){
-        return "Car has stopped";
+        return $this->model . " Car has stopped";
     }
 }
 
 $car = new Car();
 $car->color = 'white';
 $car->year = 1990;
+$car->model = "Corsa";
+
+echo $car->run();
+echo $car->stop();
+
+$car2 = new Car();
+
+$car2->color = 'red';
+$car2->year = 1995;
+
+$car2->run();
+$car2->stop();
